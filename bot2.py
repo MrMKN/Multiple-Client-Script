f24e818f3d288ac0db1ba4cc58b3595d2fd2d690
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-17 20:04:19 +0530

    Delete bot2.py

--- bot2.py
+++
@@ -1,7 +0,0 @@
-from client import bot
-from pyrogram import filters 
-
-@bot.on_message(filters.text)
-async def second_start(client, message):
-    await message.reply_text(
-        text="Hello Bro 😉 i am bot2")
