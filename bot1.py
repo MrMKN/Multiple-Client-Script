b86e39888f3124e8127574a0ff8fc7385b56d5ba
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-17 19:56:05 +0530

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,7 +1,7 @@
 from client import app
 from pyrogram import filters 
 
-@App.on_message(filters.text)
+@app.on_message(filters.text)
 async def first_start(client, message):
     await message.reply_text(
         text="Hello Bro 😉 i am bot1")
