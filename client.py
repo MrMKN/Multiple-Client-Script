e628b2882dc757d22dedbcebbc9f17d4ca073f00
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-17 18:53:23 +0530

    Update client.py

--- client.py
+++ client.py
@@ -6,8 +6,8 @@ BOT_TOKEN_2 = os.environ.get("BOT_TOKEN_2", "")
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
                         
-app1 = Client("app1", bot_token=BOT_TOKEN_2, api_id=API_ID, api_hash=API_HASH),                                           
-app2 = Client("app2", bot_token=BOT_TOKEN_1, api_id=API_ID, api_hash=API_HASH),
+app1 = Client("app1", bot_token=BOT_TOKEN_2, api_id=API_ID, api_hash=API_HASH)                                          
+app2 = Client("app2", bot_token=BOT_TOKEN_1, api_id=API_ID, api_hash=API_HASH)
        
 @app1.on_message(filters.text)
 async def first_start(client, message):
