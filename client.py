359265d3f7b0c8a6f2a3c696cb3953820296975d
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-17 18:45:38 +0530

    Update client.py

--- client.py
+++ client.py
@@ -6,16 +6,16 @@ BOT_TOKEN_2 = os.environ.get("BOT_TOKEN_2", "")
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
                         
-App1 = Client("app1", bot_token=BOT_TOKEN_2, api_id=API_ID, api_hash=API_HASH),                                           
-App2 = Client("app2", bot_token=BOT_TOKEN_1, api_id=API_ID, api_hash=API_HASH),
+app1 = Client("app1", bot_token=BOT_TOKEN_2, api_id=API_ID, api_hash=API_HASH),                                           
+app2 = Client("app2", bot_token=BOT_TOKEN_1, api_id=API_ID, api_hash=API_HASH),
        
-@App1.on_message(filters.text)
+@app1.on_message(filters.text)
 async def first_start(client, message):
     await message.reply_text(
         text="Hello Bro 😉 i am bot1")
 
 
-@App2.on_message(filters.text)
+@app2.on_message(filters.text)
 async def second_start(client, message):
     await message.reply_text(
         text="Hello Bro 😉 i am bot2")
@@ -23,7 +23,7 @@ async def second_start(client, message):
 
        
         
-App1.start()
-App2.start()
+app1.start()
+app2.start()
 
 Client.idle()
