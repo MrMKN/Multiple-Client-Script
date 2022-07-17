25c17537737152b8b20d52101e499196a82028b2
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-17 18:02:11 +0530

    Update client.py

--- client.py
+++ client.py
@@ -24,13 +24,13 @@ App2 = Client(
        api_hash=API_HASH
 )
 
-@App1.on_message(filters.command("start"))
+@App1.on_message(filters.text)
 async def first_start(client, message):
     await message.reply_text(
         text="Hello Bro 😉 i am bot1")
 
 
-@App2.on_message(filters.command("start"))
+@App2.on_message(filters.text)
 async def second_start(client, message):
     await message.reply_text(
         text="Hello Bro 😉 i am bot2")
