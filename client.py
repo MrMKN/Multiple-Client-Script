a33bf38545c8648c0b19f729b687ba0a128f5804
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-17 19:43:59 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters
+from pyrogram import Client
 import os
 
 BOT_TOKEN_1 = os.environ.get("BOT_TOKEN_1", "")
@@ -6,22 +6,13 @@ BOT_TOKEN_2 = os.environ.get("BOT_TOKEN_2", "")
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 
-App1 = Client("app1", bot_token=BOT_TOKEN_2, api_id=API_ID, api_hash=API_HASH)
-App1.run()                                          
-App2 = Client("app2", bot_token=BOT_TOKEN_1, api_id=API_ID, api_hash=API_HASH)
-App2.run()
+App = Client("app1", bot_token=BOT_TOKEN_2, api_id=API_ID, api_hash=API_HASH)
+App.start()
+                                         
+Bot = Client("app2", bot_token=BOT_TOKEN_1, api_id=API_ID, api_hash=API_HASH)
+Bot.start()
 
        
-@App1.on_message(filters.text)
-async def first_start(client, message):
-    await message.reply_text(
-        text="Hello Bro 😉 i am bot1")
-
-
-@App2.on_message(filters.text)
-async def second_start(client, message):
-    await message.reply_text(
-        text="Hello Bro 😉 i am bot2")
 
 
        
