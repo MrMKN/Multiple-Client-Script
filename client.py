f56ca3b5a85889222d8570631a2ab7fd7a7b0fa3
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-17 18:58:45 +0530

    Update client.py

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ BOT_TOKEN_2 = os.environ.get("BOT_TOKEN_2", "")
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 
-class Bot:                       
+class Bot(object):                       
     App1 = Client("app1", bot_token=BOT_TOKEN_2, api_id=API_ID, api_hash=API_HASH)                                          
     App2 = Client("app2", bot_token=BOT_TOKEN_1, api_id=API_ID, api_hash=API_HASH)
        
