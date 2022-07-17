206f9f091e312652b5108fe4c74faf89e158b62d
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-17 18:28:24 +0530

    Update client.py

--- client.py
+++ client.py
@@ -5,25 +5,13 @@ BOT_TOKEN_1 = os.environ.get("BOT_TOKEN_1", "")
 BOT_TOKEN_2 = os.environ.get("BOT_TOKEN_2", "")                             
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
-
-##--------------------[(Client 1)]----------------
-
-App1 = Client(
-       "bot1",
-       bot_token=BOT_TOKEN_1,
-       api_id=API_ID,
-       api_hash=API_HASH
-)
-
-##--------------------[(Client 2)]----------------
                         
-App2 = Client(
-       "app2",
-       bot_token=BOT_TOKEN_2,
-       api_id=API_ID,
-       api_hash=API_HASH
-)
-
+Bot = {
+    "App1": Client("app1", bot_token=BOT_TOKEN_2, api_id=API_ID, api_hash=API_HASH),                                           
+    "App2": Client("app2", bot_token=BOT_TOKEN_1, api_id=API_ID, api_hash=API_HASH),
+}
+        
+       
 @App1.on_message(filters.text)
 async def first_start(client, message):
     await message.reply_text(
@@ -37,8 +25,7 @@ async def second_start(client, message):
 
 
        
-print("bot1 starting success full 🔥....")        
-App1.run()
-print("bot2 starting success full 🔥....")
-App2.run()
+print("bot starting success full 🔥....")        
+Bot.run()
+
 
