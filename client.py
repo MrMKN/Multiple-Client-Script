35ef6ba005a866637aa1bb59872ccf72d5bf33e4
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-07-18 13:24:13 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,4 @@
 from pyrogram import Client, idle, filters
-from new.Script import START
 import os
 
 BOT_TOKEN_1 = os.environ.get("BOT_TOKEN_1", "")
@@ -9,35 +8,23 @@ API_HASH = os.environ.get("API_HASH", "")
 API_ID2 = API_ID
 API_HASH2 = API_HASH
 
+# start text
+TEXT_1 = os.environ.get("TEXT_1", "")
+TEXT_2 = os.environ.get("TEXT_2", "")
+
 app = Client("app", bot_token=BOT_TOKEN_2, api_id=API_ID, api_hash=API_HASH)
 bot = Client("bot", bot_token=BOT_TOKEN_1, api_id=API_ID2, api_hash=API_HASH2)                                       #123
          
 @app.on_message(filters.command("start"))
 async def first_start(client, message):
-    await message.reply_text(
-        text=f"""Hello {message.from_user.mention} 👋
-
-Sorry This bot is unauthorized 😔
-Please use our new bot @MKN_Hyper_renameBOT
-
-OR contact our support group @MKN_BOTZ_DISCUSSION_GROUP""")
-
-@app.on_message(filters.command("t"))
-async def test(client, message):
-    await message.reply_text(text=START)
+    await message.reply_text(text=TEXT_1)
 
 
 @bot.on_message(filters.command("start"))
 async def second_start(client, message):
-    await message.reply_text(
-        text=f"""Hello {message.from_user.mention} 👋
-
-Sorry This bot is unauthorized 😔
-Please use our new bot @MKN_ProRenameBOT
-
-OR contact our support group @PYRO_BOTZ_CHAT""")
+    await message.reply_text(text=TEXT_2)
+       
                                
-
 app.start()     
 bot.start()
 
